a2e5681e094c219f816690d9dd185b0ecae3e890
marcosfpina <sec@voidnx.com>
2026-01-17 18:05:51 -0200

    chore: initial project configuration and build setup

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "LlamaChat PoC Dev Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            protobuf # Necessário para gRPC/Prost
+          ];
+          
+          buildInputs = with pkgs; [
+            rustToolchain
+            gtk4
+            libadwaita
+            glib
+            openssl
+          ];
+          
+          # Garante que o protoc seja encontrado
+          PROTOC = "${pkgs.protobuf}/bin/protoc";
+        };
+      }
+    );
+}
